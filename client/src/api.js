a90c180c4b1af4b6dbe0856cdf952611c424ed68
MrNaveen669 <1320204008@bitraipur.ac.in>
2026-09-18 17:05:32 +0530

    mobile responsive 2

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,25 @@
-import axios from'axios';export const api=axios.create({baseURL:'/api'});api.interceptors.request.use(c=>{const t=localStorage.getItem('token');if(t)c.headers.Authorization='Bearer '+t;return c});
+import axios from 'axios';
+
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  'http://localhost:5000';
+
+export const api = axios.create({
+  baseURL: `${API_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
